ef51c571db04b2431778863ad3a5ef4d208cc663
zengbin <596659597@qq.com>
2025-01-25 11:28:05 +0800

    update

--- react-admin-app/src/pages/SACP/Mark/MarkRecord/Waterfall.jsx
+++ react-admin-app/src/pages/SACP/Mark/MarkRecord/Waterfall.jsx
@@ -4,28 +4,11 @@ import './Waterfall.scss';
 export default class Waterfall extends React.Component {
     constructor(props) {
         super(props)
-
-        const images = [
-            "images/wall/4f94970f-baaa-40fb-848f-07144a40cd08.png",
-            "images/wall/288dea35-9654-4305-b103-bc43f6f46a3b.png",
-            "images/wall/20250117172105070861.png",
-            "images/wall/c374181e-9b1d-4d1b-9b8c-0d0b38defd9a.jpeg",
-            "images/wall/c2249136-f7c1-4d34-a1f4-27f72f56b2f7.jpeg",
-            "images/wall/a1ec7808-ab5e-4f9d-974d-d12ce3f563ed.jpeg",
-            "images/wall/55a312c1-eefb-4811-bcc6-6c005281befd.jpeg",
-            "images/wall/4f94970f-baaa-40fb-848f-07144a40cd08.png",
-            "images/wall/288dea35-9654-4305-b103-bc43f6f46a3b.png",
-            "images/wall/20250117172105070861.png",
-            "images/wall/c374181e-9b1d-4d1b-9b8c-0d0b38defd9a.jpeg",
-            "images/wall/c2249136-f7c1-4d34-a1f4-27f72f56b2f7.jpeg",
-            "images/wall/a1ec7808-ab5e-4f9d-974d-d12ce3f563ed.jpeg",
-            "images/wall/55a312c1-eefb-4811-bcc6-6c005281befd.jpeg",
-        ]
-
         this.state = {
             columnMaxHeight: 0,
-            imageList: images.map(url => {
+            imageList: this.props.images.map(url => {
                 return {
+                    id: Math.ceil(Math.random() * 100000000),
                     url,
                     top: 0,
                     left: 0,
@@ -35,26 +18,48 @@ export default class Waterfall extends React.Component {
     }
 
     componentDidMount() {
-        const rootEle = this.refs['container'];
-        const {width} = rootEle.getBoundingClientRect();
-        const {columnCount, columnWidth, columnGap} = calculate(width)
-
-        console.log({width});
-
-        this.containerWidth = width
-        this.columnGap = columnGap
-        this.columnCount = columnCount
-        this.columnWidth = columnWidth
-        this.columnHeightArr = Array.apply(null, {length: this.columnCount}).map(() => 0)
-
         setTimeout(() => {
-            this.load()
-        }, 100)
+            const rootEle = this.refs['container'];
+            const {width} = rootEle.getBoundingClientRect();
+            const {columnCount, columnWidth, columnGap} = calculate(width)
 
+            this.columnGap = columnGap
+            this.columnCount = columnCount
+            this.columnWidth = columnWidth
+            this.columnHeightArr = Array.apply(null, {length: this.columnCount}).map(() => 0)
+            this.startIndex = 0;
+
+            setTimeout(() => {
+                this.load()
+            }, 100)
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.images.length > prevProps.images.length) {
+            console.log(prevProps.images.length)
+            let startIndex = prevProps.images.length;
+            let newImages = this.props.images.slice(startIndex, this.props.images.length)
+            let newItems = newImages.map(url => {
+                return {
+                    id: Math.ceil(Math.random() * 100000000),
+                    url,
+                    top: 0,
+                    left: 0,
+                }
+            })
+            this.setState({
+                imageList: this.state.imageList.concat(newItems)
+            }, () => {
+                this.load();
+            })
+        }
     }
 
     async load() {
-        for (let i = 0; i < this.state.imageList.length; i++) {
+        let i = this.startIndex;
+        for (i; i < this.state.imageList.length; i++) {
+            this.startIndex++;
             const item = this.state.imageList[i]
             const {url} = item;
             let image = await asyncLoadImage(url).catch(err => {
@@ -65,12 +70,12 @@ export default class Waterfall extends React.Component {
 
             const footerEle = this.refs[`footer${i}`];
             const react = footerEle.getBoundingClientRect();
-            console.log(footerEle, JSON.parse(JSON.stringify(react)));
+            // console.log(footerEle, JSON.parse(JSON.stringify(react)));
             const {height: footerHeight} = react;
             const scaleHeight = (this.columnWidth) / (image.width / image.height) + this.columnGap
             const itemHeight = scaleHeight + footerHeight
             // console.log(image.width, image.height, scaleHeight, footerHeight, itemHeight)
-            console.log({scaleHeight, footerHeight, itemHeight})
+            // console.log({scaleHeight, footerHeight, itemHeight})
 
             let min = Math.min(...this.columnHeightArr)
             let minIndex = this.columnHeightArr.indexOf(min)
@@ -96,8 +101,10 @@ export default class Waterfall extends React.Component {
                         width: this.columnWidth + "px",
                         left: item.left + 'px',
                         top: item.top + 'px'
-                    }} key={idx}>
-                        <img src={item.url}/>
+                    }} key={item.id}>
+                        <div className="image-wrapper">
+                            <img src={item.url}/>
+                        </div>
                         <div ref={`footer${idx}`}>
                             {footerRender(item, idx)}
                         </div>
@@ -123,7 +130,7 @@ function asyncLoadImage(url) {
 }
 
 function calculate(containerWidth) {
-    let columnCount = 3
+    let columnCount = 7
     let columnGap = 5
     let columnWidth = (containerWidth - ((columnCount - 1) * columnGap)) / columnCount
 
--- react-admin-app/src/pages/SACP/Mark/MarkRecord/index.tsx
+++ react-admin-app/src/pages/SACP/Mark/MarkRecord/index.tsx
@@ -242,6 +242,8 @@ export default () => {
         getRecordList(data);
     };
 
+    const [images, setImages] = useState(getRandomImages());
+
     return (
         <div className="mark-record">
             <Card size="small" style={{marginBottom: "16px"}}>
@@ -272,13 +274,42 @@ export default () => {
                     dataSource={markList}
                     renderItem={(item) => <MarkItem item={item} onAction={onAction} key={item.keyframe_id}/>}
                 />*/}
-                <Waterfall footerRender={(item: any, index: any) => {
+                <Waterfall images={images} footerRender={(item: any, index: any) => {
                     return <div className='footer'>
                         <p>索引 = {index}</p>
                         <p>图片地址 = {item.url}</p>
                     </div>
                 }}></Waterfall>
+
+                <div style={{textAlign: "center"}}>
+                    <button onClick={() => {
+                        setImages(images.concat(getRandomImages()));
+                    }}>加载更多
+                    </button>
+                </div>
             </Card>
         </div>
     );
 };
+
+function getRandomImages() {
+    const images = [
+        "images/wall/260275992_443951064017388_8120694513404601001_n.jpeg",
+        "images/wall/299180709_475571564022110_5374496400830654462_n.jpeg",
+        "images/wall/300303054_1084974882129458_4224607917202559964_n.jpeg",
+        "images/wall/315761796_454019013338901_3773517364246318115_n.jpeg",
+        "images/wall/319801814_154315077378726_9055366276893967024_n.jpeg",
+        "images/wall/20250117172105070861.png",
+        "images/wall/a1ec7808-ab5e-4f9d-974d-d12ce3f563ed.jpeg",
+        "images/wall/c374181e-9b1d-4d1b-9b8c-0d0b38defd9a.jpeg",
+        "images/wall/c2249136-f7c1-4d34-a1f4-27f72f56b2f7.jpeg",
+        "images/wall/4f94970f-baaa-40fb-848f-07144a40cd08.png",
+        "images/wall/55a312c1-eefb-4811-bcc6-6c005281befd.jpeg",
+        "images/wall/288dea35-9654-4305-b103-bc43f6f46a3b.png",
+        "images/wall/219349120_800300317313609_7691747447293727933_n.jpeg",
+    ]
+
+    return new Array(20).fill(null).map(() => {
+        return images[Math.floor(Math.random() * images.length)];
+    })
+}
