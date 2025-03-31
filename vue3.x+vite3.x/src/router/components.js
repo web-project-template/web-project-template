ef875d80169f85de8faf59e755dbeead1e79a929
zengbin <596659597@qq.com>
2025-03-31 17:56:37 +0800

    移动vue3+vite3

--- vue3.x+vite3.x/src/router/components.js
+++
@@ -1,6 +0,0 @@
-export default {
-  '/login': () => import('../pages/login/index.vue'),
-  '/signin': () => import('../pages/signin/index.vue'),
-  '/index': () => import('../pages/home/index.vue'),
-  '/system/user': () => import('../pages/system/user/index.vue'),
-}
